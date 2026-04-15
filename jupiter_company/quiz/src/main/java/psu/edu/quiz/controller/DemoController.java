2d0088b4919947dbf0d8b8f4f9e73b914e1f44b8
beansprout214 <brightest21@outlook.com>
2026-04-15 19:36:40 -0400

    allowing democontroller to take control of the "/" mapping (not sure if this makes much difference but i think less things will break if configured this way)

--- jupiter_company/quiz/src/main/java/psu/edu/quiz/controller/DemoController.java
+++ jupiter_company/quiz/src/main/java/psu/edu/quiz/controller/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
 	@GetMapping("/")
 	public String showHome() {
-		return "redirect:/employees/list";
+//		return "redirect:/employees/list";
+		return "redirect:/showMyLoginPage";
 	}
 }
--- jupiter_company/quiz/src/main/java/psu/edu/quiz/controller/LoginController.java
+++ jupiter_company/quiz/src/main/java/psu/edu/quiz/controller/LoginController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+//	@GetMapping("/")
+//	public String showHome() {
+//		return "redirect:/showMyLoginPage";
+//	}
 	
 	@GetMapping("/showMyLoginPage")
 	public String showMyLoginPage() {
