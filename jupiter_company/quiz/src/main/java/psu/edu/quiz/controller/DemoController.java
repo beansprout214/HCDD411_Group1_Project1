9b1cc80fe988012ba4d19682b9b1158753247daf
beansprout214 <brightest21@outlook.com>
2026-04-15 19:46:22 -0400

    changed mapping from login page to home, so localhost:8080 brings to home if logged in

--- jupiter_company/quiz/src/main/java/psu/edu/quiz/controller/DemoController.java
+++ jupiter_company/quiz/src/main/java/psu/edu/quiz/controller/DemoController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class DemoController {
 	@GetMapping("/")
 	public String showHome() {
-//		return "redirect:/employees/list";
-		return "redirect:/showMyLoginPage";
+		return "home";
 	}
 }
